92aa4a432cf2f5aa63599896defee807a6957dc7
Hung Do <hungdojan@gmail.com>
2022-03-25 15:46:27 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module ija.umleditor {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ija.umleditor to javafx.fxml;
+    exports ija.umleditor;
+    exports ija.umleditor.controllers;
+    opens ija.umleditor.controllers to javafx.fxml;
+}
