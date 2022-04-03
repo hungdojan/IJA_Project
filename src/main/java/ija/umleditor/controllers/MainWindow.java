9e97da3d6c1e8494202a15e3481bf0023dc250db
Petr Kolařík <peta.kolarik@seznam.cz>
2022-04-03 17:18:02 +0200

    Basic layout
    added FontAwesome, basic layout for class diagram

--- src/main/java/ija/umleditor/controllers/MainWindow.java
+++ src/main/java/ija/umleditor/controllers/MainWindow.java
@@ -1,14 +1,42 @@
 package ija.umleditor.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class MainWindow {
-    @FXML
-    private Label welcomeText;
+    public ImageView object;
+    public ImageView interface_final;
+    public ImageView class_final;
+    public Pane pane;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label image;
+
+    @FXML
+    public void onClicked(MouseEvent event) {
+        Group g;
+        ImageView myImage = (ImageView) event.getSource();
+        if (myImage == class_final) {
+            g = ClassDiagramFactory.createClass();
+        }
+        else if (myImage == object) {
+            g = ClassDiagramFactory.createObject();
+        }
+        else {
+            g = ClassDiagramFactory.createInterface();
+        }
+        pane.getChildren().add(g);
     }
+
+//    public void initialize() {
+//        pane.setClip(new Rectangle(300, 500));
+//    }
+
 }
